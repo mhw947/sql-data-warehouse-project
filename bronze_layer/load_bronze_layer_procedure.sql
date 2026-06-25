c70489a046b1cb6f87969004a4f1dbf8182670f0
mwang678 <mwang678@gatech.edu>
2026-06-25 11:17:33 -0700

    create procedure to bulk load csv file

--- bronze_layer/load_bronze_layer_procedure.sql
+++ bronze_layer/load_bronze_layer_procedure.sql
@@ -7,13 +7,13 @@ Purpose:
 */
 
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-BEGIN 
+-- CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+-- BEGIN 
 		-- declare variables to helps to identity bottlenecks, optimize performance, monitor trends, detect issues 
 		DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME
 		
 		-- Ensures error handling, data integrity, and issue logging for easier debugging 
-		BEGIN TRY
+		-- BEGIN TRY
 				SET @batch_start_time = GETDATE();
 				-- Add Prints to track execution, debug issues, and understand its flow 
 				PRINT '=======================================';
@@ -26,10 +26,10 @@ BEGIN
 				
 				SET @start_time = GETDATE();
 				PRINT '>> Truncating table: cust_info';
-				TRUNCATE TABLE cust_info;  -- empty table first, otherwise each time we run, it will be loaded again 
+				TRUNCATE TABLE bronze.crm_cust_info;  -- empty table first, otherwise each time we run, it will be loaded again 
 				
 				PRINT '>> Inserting data into: cust_info';
-				BULK INSERT cust_info FROM "/Users/mohanwang/Documents/Summer2026/data_engineer/sql-data-warehouse-project/datasets/source_crm/cust_info.csv" -- insert table from local csv file 
+				BULK INSERT bronze.crm_cust_info FROM "/var/opt/mssql/datasets/source_crm/cust_info.csv" -- insert table from local csv file 
 				WITH (
 				
 				FIRSTROW = 2, -- skip the first row (header) 
@@ -47,15 +47,15 @@ BEGIN
 				PRINT 'Loading Bronze Layer is Completed';
 				PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
 				PRINT '=======================================';
-		END TRY
+		-- END TRY
 		
 		-- Ensures error handling, data integrity, and issue logging for easier debugging 
-		BEGIN CATCH 
-				PRINT '=======================================';
-				PRINT 'ERRO OCCURED DURING LOADING BRONZE LAYER';
-				PRINT 'Error Message' + ERROR_MESSAGE();
-				PRINT 'ERROR Message' + CAST(ERROR_NUMBER() AS NVARCHAR);
-				PRINT 'ERROR Message' + CAST(ERROR_STATE() AS NVARCHAR);
-				PRINT '=======================================';
-		END CATCH
-END
+		-- BEGIN CATCH 
+		-- 		PRINT '=======================================';
+		-- 		PRINT 'ERRO OCCURED DURING LOADING BRONZE LAYER';
+		-- 		PRINT 'Error Message' + ERROR_MESSAGE();
+		-- 		PRINT 'ERROR Message' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		-- 		PRINT 'ERROR Message' + CAST(ERROR_STATE() AS NVARCHAR);
+		-- 		PRINT '=======================================';
+		-- END CATCH
+-- END
