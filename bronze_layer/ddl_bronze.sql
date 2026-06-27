97e1a10032a58bf08a058d35914aa1cf77af333d
mwang678 <mwang678@gatech.edu>
2026-06-26 21:27:07 -0700

    create table for erp tables and load data for bronze layer

--- bronze_layer/ddl_bronze.sql
+++ bronze_layer/ddl_bronze.sql
@@ -37,7 +37,7 @@ GO
 
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details;
-GO 
+GO
 
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num     NVARCHAR(50),
@@ -50,4 +50,37 @@ CREATE TABLE bronze.crm_sales_details (
     sls_quantity    INT,
     sls_price       INT
 );
-GO 
+GO
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid             NVARCHAR(50),
+    bdate           DATE,
+    gen             NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid             NVARCHAR(50),
+    cntry           NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50)
+);
+GO
